8bd0b613f9d9f76d31235b9f7065ec67740853d3
Exaucé Benjamin Kyalondawa <exaucebenjamink@gmail.com>
2024-05-17 14:32:53 +0100

    menu berger solution

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 let menuBurger = document.querySelector(".menuBerger");
 let hiddenSection = document.querySelector(".addContact");
 let infoSction = document.querySelector("#infoContactsid");
+let corbeilleEditvisible = document.querySelector(
+  ".manageIconDeleteEdit--opacity"
+);
+let clickLibelle = document.querySelector(".iconLibelle--cursorpointer");
+let createNewLibelle = document.querySelector(".creatLibelle");
+console.log(createNewLibelle);
+const body = document.getElementsByTagName("body");
+
+// ------------------// click sur menu burger
 
 menuBurger.addEventListener("click", function () {
-  if (hiddenSection.style.display === "none") {
-    hiddenSection.style.display = "inline-block";
-    infoSction.style.width = "78%";
-    hiddenSection.style.transform = "translate(0)";
+  console.log(hiddenSection);
+  hiddenSection.classList.toggle("show");
+  infoSction.classList.toggle("infoContactsTransition");
+});
+
+// ---------------------survole sur college et Restaurer... afficher le corbeille et edit
+
+corbeilleEditvisible.addEventListener("mousemove", function () {
+  if (corbeilleEditvisible.style.opacity === "0") {
+    corbeilleEditvisible.style.opacity = "0";
   } else {
-    hiddenSection.style.display = "none";
-    infoSction.style.width = "100%";
-    hiddenSection.style.transform = "translate(-100%)";
-    hiddenSection.style.transition= "0.5s";
+    corbeilleEditvisible.style.opacity = "1";
   }
+});
+
+// -------------- au click sur libelle
+clickLibelle.addEventListener("click", function () {
+  createNewLibelle.addEventListener.style.display = "block";
+  // body.addEventListener.style.overflow = "hidden";
 });
